a5677932d07a1721ddfa8b27a3b37cec6fee490c
brandalik <brandalik@eit.uni-kl.de>
2018-04-16 16:51:40 +0200

    The SQL Commant can now consist of more than one line. Made the functions more compact.

--- Data/Subfunctions/Matlab2Access_ExecuteSQL.m
+++ Data/Subfunctions/Matlab2Access_ExecuteSQL.m
@@ -1,4 +1,4 @@
-function Done = Matlab2Sincal_ExecuteSQL(sql_in, DB_Name, DB_Path, DB_Type)
+function Done = Matlab2Access_ExecuteSQL(sql_in, DB_Name, DB_Path, DB_Type)
 % AccessDelValues - Delete all Values in Tab_Name Table from DB_Name
 %                   Database
 %
@@ -16,16 +16,11 @@ function Done = Matlab2Sincal_ExecuteSQL(sql_in, DB_Name, DB_Path, DB_Type)
 
 %% Input check
 
-if nargin < 3                   % Set the default path
-    DB_Path = [cd,'\'];
-end
-if nargin < 4                   % Set the default database typ
-    DB_Type = '.accdb';
-end
-if DB_Path(end) ~= '\'          % Correct the path if necessary
-    DB_Path = [DB_Path,'\'];
-end
-
+if nargin < 3;          DB_Path = [cd,'\'];       end	% Set the default path
+if nargin < 4;          DB_Type = '.accdb';       end	% Set the default database typ
+if DB_Path(end) ~= '\'; DB_Path = [DB_Path,'\'];  end   % Correct the path if necessary
+if ischar(sql_in);      sql_in = cellstr(sql_in); end   % if sql-input is the class char 
+    
 %% Create the Matlab connection with the Access Database
 
 provider  = 'Microsoft.ACE.OLEDB.12.0';                                     % Define the Provider
@@ -35,17 +30,20 @@ max_tries = 10;
 k_try     = 1 ;
 while Done == false && k_try < max_tries
     try
-        srv = actxserver('ADODB.connection'  ); % Create a local OLE Automation server "srv" for starting the Access process
-        invoke(srv,'Open'        , open_comm );	% Open the connection with the Access Database
-        invoke(srv,'BeginTrans'              ); % Begin a new transaction in the open DB connection
-        invoke(srv,'Execute'     , sql_in    ); % invoke the changes
-        invoke(srv,'CommitTrans'             ); % Save all changes
-        invoke(srv,'Close'                   ); % Close the Matlab connection with the Access Database
-        srv = []; %#ok                       	% Set the srv variable to Nothing
+        srv = actxserver('ADODB.connection'  );             % Create a local OLE Automation server "srv" for starting the Access process
+        invoke(srv,'Open'        , open_comm );             % Open the connection with the Access Database
+        invoke(srv,'BeginTrans'              );             % Begin a new transaction in the open DB connection
+        for sql_in_k = 1 : size(sql_in,1)                   % invoke the changes
+            invoke(srv,'Execute' , sql_in{sql_in_k,1}    ); 
+        end
+        invoke(srv,'CommitTrans'             );             % Save all changes
+        invoke(srv,'Close'                   );             % Close the Matlab connection with the Access Database
+        srv = []; %#ok                                      % Set the srv variable to Nothing
         Done = true;
         fprintf(['Execution try ',num2str(k_try),' in ',DB_Name,' successful.\n']);
     catch
         fprintf(['Execution try ',num2str(k_try),'/',num2str(max_tries),' in ',DB_Name,' was not successful.\n']);
+        k_try = k_try + 1;
         if k_try == max_tries
             fprintf(['Connection with ',DB_Name,' could not be created. Programm will stop.\n']);
             return;
