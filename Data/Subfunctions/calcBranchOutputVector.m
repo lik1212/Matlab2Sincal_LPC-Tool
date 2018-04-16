4a03b7dbaa7daf7bb9b1d8a045ada5c9ec7fce63
brandalik <brandalik@eit.uni-kl.de>
2018-04-16 15:18:25 +0200

    Made the functions more compact

--- Data/Subfunctions/calcBranchOutputVector.m
+++ Data/Subfunctions/calcBranchOutputVector.m
@@ -1,30 +1,14 @@
 function Vector = calcBranchOutputVector(options)
 % Determines which branch results values to output from Sincal Database
-% For example only I or I,S_flow,P_flow,Q_flow and so on
+% For example only I or I, S_flow, P_flow, Q_flow and so on
 %
-% Author(s): P. Gassler
+% Author(s): P. Gassler, R. Brandalik
 
-Col_Name = load('Col_Name_ULFBranchResult.mat');
+% Check Data\Static_Input\Col_Name_ULFBranchResult.mat for more information
 
-% Vector = [2,3,5,6,8,11,12,14,17,18,20,41];
-Vector = [2,3,41];
-
-if options.I
-    Vector = [Vector,8,14,20];
-end
-
-if options.P_flow
-    Vector = [Vector,5,11,17];
-end
-
-if options.Q_flow
-    Vector = [Vector,6,12,18];
-end
-
-if options.S_flow
-    Vector = [Vector,7,13,19];
-end
-
+Vector = [2, 3, 41]; % Terminal1_ID, Terminal2_ID, ResTime
+if options.P_flow; Vector = [Vector, 5, 11, 17]; end
+if options.Q_flow; Vector = [Vector, 6, 12, 18]; end
+if options.S_flow; Vector = [Vector, 7, 13, 19]; end
+if options.I     ; Vector = [Vector, 8, 14, 20]; end
 Vector = sort(Vector);
-
-end
--- Data/Subfunctions/calcNodeOutputVector.m
+++ Data/Subfunctions/calcNodeOutputVector.m
@@ -1,35 +1,15 @@
 function Vector = calcNodeOutputVector(options)
 % Determines which node results values to output from Sincal Database
-% For example only U or U,S,P,Q and so on
+% For example only U or U, S, P, Q and so on
 %
-% Author(s): P. Gassler
+% Author(s): P. Gassler, R. Brandalik
 
+% Check Data\Static_Input\Col_Name_ULFNodeResult.mat for more information
 
-Col_Name = load('Col_Name_ULFNodeResult.mat');
-
-% Vector = [2,4,6,7,8,9,10,12,13,14,15,16,18,19,20,21,22,31,47];
-Vector = [2,47];
-
-if options.U
-    Vector = [Vector,4,10,16,22];
-end
-
-if options.S
-    Vector = [Vector,9,15,21,31];
-end
-
-if options.P
-    Vector = [Vector,7,13,19];
-end
-
-if options.Q
-    Vector = [Vector,8,14,20];
-end
-
-if options.phi
-    Vector = [Vector,6,12,18];
-end
-
+Vector = [2, 47];   % Node_ID & ResTime
+if options.U  ; Vector = [Vector, 4, 10, 16, 22]; end
+if options.phi; Vector = [Vector, 6, 12, 18    ]; end
+if options.P  ; Vector = [Vector, 7, 13, 19    ]; end
+if options.Q  ; Vector = [Vector, 8, 14, 20    ]; end
+if options.S  ; Vector = [Vector, 9, 15, 21, 31]; end
 Vector = sort(Vector);
-
-end
