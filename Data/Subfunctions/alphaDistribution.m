c9fa373cccbb14d045edc1dab5934e03b08cf62d
brandalik <brandalik@eit.uni-kl.de>
2018-04-17 15:02:28 +0200

    From 3 Phase option only to 1 Phase option. TODO: Add comments.

--- Data/Subfunctions/alphaDistribution.m
+++ Data/Subfunctions/alphaDistribution.m
@@ -1,40 +1,28 @@
-function Dist_List = alphaDistribution(SinInfo,Profiles_Names)
+function Dist_List = alphaDistribution(SinInfo, Profiles_Names)
 %
+%   Distribution List alphabetical sorted
 %
-%
-% Author(s): P. Gassler
+% Author(s): P. Gassler, R. Brandalik
 
-if nargin~=2
-   error('Too many or too few arguments'); 
-end
+if nargin~=2; error('Wrong number of input arguments'); end
 
 Dist_List = table;
-Grid_Load = sortrows(SinInfo.Load.Name);
-Load_Profile = cell(size(SinInfo.Load,1),1);
-nb_loads = size(SinInfo.Load,1);
-nb_loads_3p = nb_loads / 3;
-
-nb_profiles = numel(Profiles_Names);
-nb_profiles_3p = nb_profiles / 3;
-Profiles_Names = sortrows(Profiles_Names);
-nb_k = ceil(nb_loads_3p/nb_profiles_3p);
-
-for k = 1 : nb_k
-    if k ~= nb_k
-        Load_Profile(1 + (k-1) * nb_profiles:nb_profiles * k) = Profiles_Names();
+Profiles_Names = sort(Profiles_Names);
+if isfield(SinInfo,'Load')
+    Grid_Load = sortrows(SinInfo.Load.Name);
+    if numel(Profiles_Names) > numel(Grid_Load)
+        Dist_List.Grid_Load    = Grid_Load;
+        Dist_List.Load_Profile = Profiles_Names(1:numel(Grid_Load));
+    elseif numel(Profiles_Names) > 0
+        mult_Profiles_Names = ceil(numel(Grid_Load)/numel(Profiles_Names));
+        Profiles_Names = repmat(Profiles_Names,mult_Profiles_Names,1);
+        Dist_List.Grid_Load    = Grid_Load;
+        Dist_List.Load_Profile = Profiles_Names(1:numel(Grid_Load));
     else
-        Load_Profile(1 + (k-1) * nb_profiles:end) = Profiles_Names(1:nb_loads - (nb_k-1)*nb_profiles);
+        Dist_List.Grid_Load    = zeros(0);
+        Dist_List.Load_Profile = zeros(0);
     end
-end
-
-Dist_List.Grid_Load = cell(nb_loads,1);
-Dist_List.Load_Profile = cell(nb_loads,1);
-dist = zeros(nb_loads,1);
-dist(1:1:nb_loads_3p) = [1:3:nb_loads];
-dist(nb_loads_3p + 1:1:nb_loads_3p*2) = [2:3:nb_loads];
-dist(nb_loads_3p*2 + 1:1:nb_loads) = [3:3:nb_loads];
-
-Dist_List.Grid_Load = Grid_Load(dist);
-Dist_List.Load_Profile = Load_Profile(dist);
-
+else
+    Dist_List.Grid_Load    = zeros(0);
+    Dist_List.Load_Profile = zeros(0);
 end
