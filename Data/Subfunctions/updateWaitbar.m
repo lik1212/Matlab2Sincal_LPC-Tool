65130502708fcd59b62c6623c2d56b59f716984b
Robert Brandalik <Robert Brandalik@131.246.82.189>
2018-04-21 18:59:42 +0200

    Prepering for deleting this function

--- Data/Subfunctions/updateWaitbar.m
+++ Data/Subfunctions/updateWaitbar.m
@@ -1,4 +1,4 @@
-function cancel = updateWaitbar(flag,hand,val,message)
+function updateWaitbar(flag, hand, val, message)
 % Updates the progress and message from a Waitbar or deletes a Waitbar.
 % Also verifies if user aborted the process by clicking the cancel button.
 %  
@@ -7,17 +7,7 @@ function cancel = updateWaitbar(flag,hand,val,message)
 
 switch flag
     case 'update'
-        waitbar(val,hand,message)
-        if getappdata(hand,'canceling')
-                delete(hand);
-                cancel =  true;
-                disp('Process stopped by User!');
-        else
-            cancel = false;
-        end
-        
+        waitbar(val, hand, message)        
     case 'delete'
         delete(hand);
-        cancel = true;
-end
 end
