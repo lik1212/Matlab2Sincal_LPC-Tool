81a29886c79212a1434fdb18c7947cca650763d6
Robert Brandalik <Robert Brandalik@131.246.81.219>
2018-04-22 19:42:48 +0200

    preperation for bundle into one function

--- Data/Subfunctions/randomDistribution.m
+++ Data/Subfunctions/randomDistribution.m
@@ -1,22 +1,22 @@
-function Dist_List = randomDistribution(SinInfo, Profiles_Names)
+function Dist_List = randomDistribution(SinInfo, ElementType ,Profiles_Names)
 %
 %   Distribution List randomly sorted
 %
 % Author(s): P. Gassler, R. Brandalik
 
-if nargin ~= 2; error('Too many or too few arguments'); end
+if nargin ~= 3; error('Too many or too few arguments'); end
 
 Dist_List = table;
-if isfield(SinInfo,'Load')
-    Grid_Load = sortrows(SinInfo.Load.Name);
-    if numel(Profiles_Names) > numel(Grid_Load)
-        Dist_List.Grid_Load    = Grid_Load;
-        Dist_List.Load_Profile = Profiles_Names(randperm(numel(Profiles_Names),numel(Grid_Load)));
+if isfield(SinInfo, ElementType)
+    Grid_Element = sortrows(SinInfo.(ElementType).Name);
+    if numel(Profiles_Names) > numel(Grid_Element)
+        Dist_List.Grid_Load    = Grid_Element;
+        Dist_List.Load_Profile = Profiles_Names(randperm(numel(Profiles_Names),numel(Grid_Element)));
     elseif numel(Profiles_Names) > 0
-        mult_Profiles_Names = ceil(numel(Grid_Load)/numel(Profiles_Names));
+        mult_Profiles_Names = ceil(numel(Grid_Element)/numel(Profiles_Names));
         Profiles_Names = repmat(Profiles_Names,mult_Profiles_Names,1);
-        Dist_List.Grid_Load    = Grid_Load;
-        Dist_List.Load_Profile = Profiles_Names(randperm(numel(Profiles_Names),numel(Grid_Load)));
+        Dist_List.Grid_Load    = Grid_Element;
+        Dist_List.Load_Profile = Profiles_Names(randperm(numel(Profiles_Names),numel(Grid_Element)));
     else
         Dist_List.Grid_Load    = zeros(0);
         Dist_List.Load_Profile = zeros(0);
