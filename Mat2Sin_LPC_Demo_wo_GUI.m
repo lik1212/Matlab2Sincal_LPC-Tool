e773dd49702699ec18587c22eb31dd02fb45d93a
brandalik <brandalik@eit.uni-kl.de>
2018-04-23 11:02:43 +0200

    Demonstration File will change from time to time

--- Mat2Sin_LPC_Demo_wo_GUI.m
+++ Mat2Sin_LPC_Demo_wo_GUI.m
@@ -10,18 +10,18 @@ Inputs.LP_DB_Path         = [pwd, '\Inputs\Load_Profiles\'         ] ; % Path fo
 Inputs.LP_DB_Type         = 'DB'                                     ;
 Inputs.LP_DB_Name         = 'IEEE_Lo_Profiles.mat'                   ;
 Inputs.LP_dist_path       = [pwd, '\Inputs\Profiles_Distribution\' ] ; % Path for the L.P. distribution file
-Inputs.LP_dist_type       = 'random'                                   ;    
+Inputs.LP_dist_type       = 'DB_order'                               ;    
 % Inputs.LP_dist_list_name  = 'LoadName_IEEE_LV_Profiles.txt'          ; 
 Inputs.PV_DB_Path         = [pwd, '\Inputs\PV_Profiles\'           ] ; % Path for PV profiles
 Inputs.PV_DB_Type         = 'DB'                                     ;
 Inputs.PV_DB_Name         = 'IEEE_PV_Profiles.mat'                   ;
 Inputs.PV_dist_path       = [pwd, '\Inputs\Profiles_Distribution\' ] ; % Path for the PV.P. distribution file
-Inputs.PV_dist_type       = 'random'                                   ;
+Inputs.PV_dist_type       = 'DB_order'                               ;
 % Inputs.PV_dist_list_name  = 'DCInfeederName_IEEE_LV_Profiles.txt'    ;
 
 Inputs.VerSincal          = 13  ;
 Inputs.ParrallelCom       = true;
-Inputs.NumelCores         = 10  ;
+Inputs.NumelCores         = 3  ;
 % Inputs.waitbar_activ      = false;
 
 Inputs.Outputs_Path       = [pwd, '\Outputs\'         ]   ; % Path for load profiles
