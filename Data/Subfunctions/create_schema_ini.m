b80a9d8cb9e3862f47c96803275b3a89c7aa3e15
brandalik <brandalik@eit.uni-kl.de>
2018-04-23 17:37:48 +0200

    temp version

--- Data/Subfunctions/create_schema_ini.m
+++ Data/Subfunctions/create_schema_ini.m
@@ -1,56 +1,52 @@
-function create_schema_ini(type,Sin_Path,num_grids,instants_per_grid,SinNameEmpty)
+function create_schema_ini(File_Type, File_Path, num_grids, instants_per_grid, SinNameEmpty)
 %create_schema_ini Creates schema.ini files
 %
-%
-%
-
 %   Author(s): P. Gassler
 %              R. Brandalik
 %              J. Greiner
 
+%% Main
 
-% if ~isdir(Sin_Path); mkdir(Sin_Path); end % verify and create folder if does not exists
-if exist([Sin_Path,'schema.ini'],'file'); delete([Sin_Path,'schema.ini']); end % delete old schema.ini file if exist
-fileID = fopen([Sin_Path,'schema.ini'],'at');% create and open schema.ini file
+if exist([File_Path,'schema.ini'],'file'); delete([File_Path,'schema.ini']); end % delete old schema.ini file if exist
 
-switch type
-    case 'input'
-        for k_grid = 1:num_grids % over all grids for all important tables
+fileID = fopen([File_Path,'schema.ini'],'at'); % create and open schema.ini file
+
+for k_grid = 1 : num_grids % over all grids for all important tables
+    switch File_Type
+        case 'input'
             % read-in files from txt to Access
-            fprintf(fileID,['[OpSer_',num2str(instants_per_grid),'inst_',num2str(k_grid),'.txt]\n']);
-            fprintf(fileID,'Format=Delimited(;)\n');
-            fprintf(fileID,'DecimalSymbol=.\n');
-            fprintf(fileID,['[OpSerVal_',num2str(instants_per_grid),'inst_',num2str(k_grid),'.txt]\n']);
-            fprintf(fileID,'Format=Delimited(;)\n');
-            fprintf(fileID,'DecimalSymbol=.\n');
             % Important for right read-in of txt into Access
-            fprintf(fileID,'Col1="OpSerVal_ID" Integer\n');
-            fprintf(fileID,'Col2="OpSer_ID" Integer\n');
-            fprintf(fileID,'Col3="OpTime" Double\n');
-            fprintf(fileID,'Col4="Flag_Curve" Integer\n');
-            fprintf(fileID,'Col5="Factor" Double\n');
-            fprintf(fileID,'Col6="P" Double\n');
-            fprintf(fileID,'Col7="Q" Double\n');
-            fprintf(fileID,'Col8="Variant_ID" Integer\n');
-            fprintf(fileID,'Col9="Flag_Variant" Integer\n');
-            fprintf(fileID,'Col10="Op_ID" Integer\n');
-        end
-    case 'output'
-        for k_grid = 1:num_grids % over all grids for all important tables
+            file_suffix = [num2str(instants_per_grid),'inst_',num2str(k_grid),'.txt]'];
+            fprintf(fileID, ['[OpSer_'   , file_suffix,   '\n']);
+            fprintf(fileID, 'Format        = Delimited(;)  \n' );
+            fprintf(fileID, 'DecimalSymbol = .             \n' );
+            fprintf(fileID, ['[OpSerVal_', file_suffix,   '\n']);
+            fprintf(fileID, 'Format        = Delimited(;)  \n' );
+            fprintf(fileID, 'DecimalSymbol = .             \n' );
+%             fprintf(fileID, 'Col1          = "OpSerVal_ID"  Integer \n');
+%             fprintf(fileID, 'Col2          = "OpSer_ID"     Integer \n');
+%             fprintf(fileID, 'Col3          = "OpTime"       Double  \n');
+%             fprintf(fileID, 'Col4          = "Flag_Curve"   Integer \n');
+%             fprintf(fileID, 'Col5          = "Factor"       Double  \n');
+%             fprintf(fileID, 'Col6          = "P"            Double  \n');
+%             fprintf(fileID, 'Col7          = "Q"            Double  \n');
+%             fprintf(fileID, 'Col8          = "Variant_ID"   Integer \n');
+%             fprintf(fileID, 'Col9          = "Flag_Variant" Integer \n');
+%             fprintf(fileID, 'Col10         = "Op_ID"        Integer \n');
+        case 'output'
             % read-in files from txt to Acces
             % Branch and Node Results from Access to txt
-            fprintf(fileID,['[BranchRes_',SinNameEmpty(1:end-6),'_',num2str(instants_per_grid),'inst_',num2str(k_grid),'.txt]\n']);
-            fprintf(fileID,'Format=Delimited(;)\n');
-            fprintf(fileID,'NumberDigits = 15\n');
-            fprintf(fileID,'DecimalSymbol=.\n');
-            fprintf(fileID,['[NodeRes_',SinNameEmpty(1:end-6),'_',num2str(instants_per_grid),'inst_',num2str(k_grid),'.txt]\n']);
-            fprintf(fileID,'Format=Delimited(;)\n');
-            fprintf(fileID,'NumberDigits = 15\n');
-            fprintf(fileID,'DecimalSymbol=.\n');
-        end
+            file_suffix = [SinNameEmpty(1:end - 6),'_',num2str(instants_per_grid),'inst_',num2str(k_grid),'.txt]'];
+            fprintf(fileID,['[BranchRes_', file_suffix, '\n']);
+            fprintf(fileID,'Format        = Delimited(;) \n' );
+            fprintf(fileID,'NumberDigits  = 15           \n' );
+            fprintf(fileID,'DecimalSymbol = .            \n' );
+            fprintf(fileID,['[NodeRes_',   file_suffix, '\n']);
+            fprintf(fileID,'Format        = Delimited(;) \n' );
+            fprintf(fileID,'NumberDigits  = 15           \n' );
+            fprintf(fileID,'DecimalSymbol = .            \n' );
+        otherwise
+            error('Unknown schema.ini type.');
+    end
 end
-% close schema.ini file
-fclose(fileID);
-
-
-end
+fclose(fileID); % close schema.ini file
