6e18b0212bfbfecce640084000b0f2bee27e9d14
brandalik <brandalik@eit.uni-kl.de>
2018-04-23 19:54:34 +0200

    Code rearranged

--- Data/Subfunctions/Matlab2Access_ExecuteSQL.m
+++ Data/Subfunctions/Matlab2Access_ExecuteSQL.m
@@ -30,15 +30,15 @@ max_tries = 10;
 k_try     = 1 ;
 while Done == false && k_try <= max_tries
     try
-        srv = actxserver('ADODB.connection'  );             % Create a local OLE Automation server "srv" for starting the Access process
-        invoke(srv,'Open'        , open_comm );             % Open the connection with the Access Database
-        invoke(srv,'BeginTrans'              );             % Begin a new transaction in the open DB connection
-        for sql_in_k = 1 : size(sql_in,1)                   % invoke the changes
-            invoke(srv,'Execute' , sql_in{sql_in_k,1}    ); 
+        srv = actxserver('ADODB.connection'   );             % Create a local OLE Automation server "srv" for starting the Access process
+        invoke(srv, 'Open'        , open_comm );             % Open the connection with the Access Database
+        invoke(srv, 'BeginTrans'              );             % Begin a new transaction in the open DB connection
+        for sql_in_k = 1 : size(sql_in,1)                    % invoke the changes
+            invoke(srv, 'Execute' , sql_in{sql_in_k,1}); 
         end
-        invoke(srv,'CommitTrans'             );             % Save all changes
-        invoke(srv,'Close'                   );             % Close the Matlab connection with the Access Database
-        srv = []; %#ok                                      % Set the srv variable to Nothing
+        invoke(srv, 'CommitTrans'             );             % Save all changes
+        invoke(srv, 'Close'                   );             % Close the Matlab connection with the Access Database
+        srv = []; %#ok                                       % Set the srv variable to Nothing
         Done = true;
         fprintf(['Execution try ',num2str(k_try),' in ',DB_Name,' successful.\n']);
     catch
