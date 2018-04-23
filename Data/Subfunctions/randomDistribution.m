281d26f10cde2cc7bc2d97c987d83b10c3c304c1
brandalik <brandalik@eit.uni-kl.de>
2018-04-23 10:58:28 +0200

    Small correction in how the profiles are sorted. Add the option to order loads acording to the order in the profile database.

--- Data/Subfunctions/randomDistribution.m
+++ Data/Subfunctions/randomDistribution.m
@@ -19,17 +19,18 @@ if isfield(SinInfo, ElementType) && num_Profiles > 0
     if num_Grid_Loads > num_Profiles
         % If less profiles than grids loads occur repeat the profiles
         mult_Profiles_Names = ceil(num_Grid_Loads/num_Profiles);
-        Profiles_Names      = sort  (Profiles_Names)                        ;
         Profiles_Names      = repmat(Profiles_Names, mult_Profiles_Names, 1);
     end
     switch dist_type
-        case 'random' % Randomly sorted
-            Dist_List.Load_Profile = Profiles_Names(randperm(num_Profiles, num_Grid_Loads));
-        case 'alphab' % Alphabetical sorted
-            Dist_List.Load_Profile = Profiles_Names(1:num_Grid_Loads);
+        case 'random'   % Randomly sorted
+            Profiles_Names = Profiles_Names(randperm(num_Profiles, num_Grid_Loads));
+        case 'alphab'   % Alphabetical sorted
+            Profiles_Names = sort(Profiles_Names);
+        case 'DB_order' % Database order sorted
         otherwise
             error('Unknown Distribution Type.');
     end
+    Dist_List.Load_Profile = Profiles_Names(1:num_Grid_Loads);
 else
     % If no grid loads nor profiles occur
     Dist_List.Grid_Load    = zeros(0);
