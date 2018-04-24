43b1ea59633c6e4d6d556f785c235f187bf842a7
brandalik <brandalik@eit.uni-kl.de>
2018-04-24 16:30:43 +0200

    Revision

--- Data/Subfunctions/create_OpSerVal_txt.m
+++ Data/Subfunctions/create_OpSerVal_txt.m
@@ -1,48 +1,37 @@
-function create_OpSerVal_txt(LoadProfile,Sin_PathInput,OpSer_suffix)
-%create_txt Create OpSerVal txt files to be read in the Sincal DB
-%   create_txt(LoadProfile,k_grid) creates the OpSer and OpSerVal txt files
-%   to be read in the Sincal Database based on the load profiles in
-%   LoadProfile and for the k_grid in the path
-
+function create_OpSerVal_txt(Profile_DB, File_Path, File_suffix)
+%create_OpSerVal_txt Create OpSerVal txt file to be read into the Sincal DB
+%   create_OpSerVal_txt(Profile_DB, File_Path, File_suffix) creates the txt
+%   file to be read into the Sincal Database table OpSerVal to add new
+%   profiles. The Table OpSerVal contains the power values of the profiles.
+%
 %   Author(s): J. Greiner
 %              R. Brandalik
 
 %% Create OpSerVal table
+% Details about OpSerVal table can be find in the Sincal DB user manual
 
-num_of_profiles     = numel(fieldnames(LoadProfile));                         % number of profiles in OpSer table, number of load*3 (phase L1, L2, L3)
-fieldnames_DB       = fieldnames(LoadProfile);               	% fieldnames will be given for load profile names
-instants_per_grid   = size(LoadProfile.(fieldnames_DB{1}),1);
+OpSer_Name   = fieldnames(Profile_DB)                       ; % Profile names
+num_profil   = numel(OpSer_Name)                            ; % number of profiles
+num_instants = unique(structfun(@(x) size(x,1), Profile_DB)); % number of instants (time steps)
 
-table_OpSerVal = table;                                         % initial OpSerVal table
-P_values       = zeros(instants_per_grid*num_of_profiles,1);    % initial P_values for all load profiles
-Q_values       = zeros(instants_per_grid*num_of_profiles,1);    % initial Q_values for all load profiles
-
-% fillup P and Q values
-for k_fieldnames = 1:numel(fieldnames_DB)
-    P_values((k_fieldnames-1)*instants_per_grid + 1:...
-        (k_fieldnames)*instants_per_grid)  = ...
-        LoadProfile.(fieldnames_DB{k_fieldnames}).P;
-    Q_values((k_fieldnames-1)*instants_per_grid + 1:...
-        (k_fieldnames)*instants_per_grid)  = ...
-        LoadProfile.(fieldnames_DB{k_fieldnames}).Q;
-end
-
+% fillup P and Q values consecutive as one column
+P_values = reshape(struct2array(structfun(@(x) x.P(:), Profile_DB, 'UniformOutput', 0)),[],1);
+Q_values = reshape(struct2array(structfun(@(x) x.Q(:), Profile_DB, 'UniformOutput', 0)),[],1);
 % NaN to zero
-P_values(isnan(P_values))   = 0;
-Q_values(isnan(Q_values))   = 0;
-% Table OpSer setup and fillup, more details in the Sincal Database user manual
-table_OpSerVal.OpSerVal_ID  = (1:instants_per_grid*num_of_profiles)'; % Primary Key
-table_OpSerVal.OpSer_ID     = kron(1:num_of_profiles,ones(1,instants_per_grid))';
-table_OpSerVal.OpTime       = repmat(1:instants_per_grid,1,k_fieldnames)';
-table_OpSerVal.Flag_Curve   = ones(instants_per_grid*num_of_profiles,1);
-table_OpSerVal.Factor       = ones(instants_per_grid*num_of_profiles,1);
-table_OpSerVal.P            = P_values;
-table_OpSerVal.Q            = Q_values;
-table_OpSerVal.Variant_ID   = ones(instants_per_grid*num_of_profiles,1);
-table_OpSerVal.Flag_Variant = ones(instants_per_grid*num_of_profiles,1);
-table_OpSerVal.Op_ID        = ones(instants_per_grid*num_of_profiles,1);
+P_values(isnan(P_values)) = 0;
+Q_values(isnan(Q_values)) = 0;
+
+table_OpSerVal              = table                                     ; % initial OpSerVal table
+table_OpSerVal.OpSerVal_ID  = double (1:num_instants * num_profil    )' ; % Primary Key
+table_OpSerVal.OpSer_ID     = repelem(1:num_profil  , 1, num_instants)' ;
+table_OpSerVal.OpTime       = repmat (1:num_instants, 1, num_profil  )' ;
+table_OpSerVal.Flag_Curve   = ones   (num_instants * num_profil,1)      ;
+table_OpSerVal.Factor       = ones   (num_instants * num_profil,1)      ;
+table_OpSerVal.P            = P_values                                  ;
+table_OpSerVal.Q            = Q_values                                  ;
+table_OpSerVal.Variant_ID   = ones   (num_instants * num_profil, 1)     ;
+table_OpSerVal.Flag_Variant = ones   (num_instants * num_profil, 1)     ;
+table_OpSerVal.Op_ID        = ones   (num_instants * num_profil, 1)     ;
 
 % Save table
-writetable(table_OpSerVal,[Sin_PathInput,'OpSerVal',OpSer_suffix,'.txt'],'Delimiter',';','QuoteStrings',false);
-
-end
+writetable(table_OpSerVal, [File_Path, 'OpSerVal', File_suffix, '.txt'], 'Delimiter', ';', 'QuoteStrings', false);
--- Data/Subfunctions/create_OpSer_txt.m
+++ Data/Subfunctions/create_OpSer_txt.m
@@ -1,39 +1,35 @@
-function create_OpSer_txt(fieldnames_DB,Sin_PathInput,OpSer_suffix) % TODO, Comments anpassen
-%create_txt Create OpSer txt files to be read in the Sincal DB
-%   create_OpSer_txt(LoadProfile,k_grid) creates the txt files
-%   to be read in the Sincal Database based on the load profiles in
-%   LoadProfile and for the k_grid in the path
-
+function create_OpSer_txt(OpSer_Name, File_Path, File_suffix)
+%create_OpSer_txt Create OpSer txt file to be read into the Sincal DB
+%   create_OpSer_txt(OpSer_Name, File_Path, File_suffix) creates the txt
+%   file to be read into the Sincal Database table OpSer to add new
+%   profiles. The Table OpSer contains the basic information about the
+%   profiles.
+%
 %   Author(s): J. Greiner
 %              R. Brandalik
 
 %% Create OpSer table
+% Details about OpSer table can be find in the Sincal DB user manual
 
-table_OpSer         = table;                                                    % initial OpSer table
-num_of_profiles     = numel(fieldnames_DB);                                     % number of profiles in OpSer table, number of load*3 (phase L1, L2, L3)
-OpSer_Name          = cellfun(@(x) x(6:end), fieldnames_DB,'UniformOutput',0);	% Without 'Load_' or 'PV___'
-OpSer_Shortname     = strcat(repmat('P',num_of_profiles,1),...                  % shortname as ['P_', "running counter variable"]
-    num2str((1:num_of_profiles)'));                                             
-OpSer_Shortname     = strrep(cellstr(OpSer_Shortname),' ','0');                 % ...
+num_profil      = numel(OpSer_Name)                                       ; % number of profiles in OpSer table
+OpSer_Shortname = strrep(strcat({'P_'}, num2str((1:num_profil)')),' ','0'); % shortname as ['P_', "running counter variable"]
 
-% Table OpSer setup and fillup, more details in the Sincal Database user manual
-table_OpSer.OpSer_ID     =     (1:num_of_profiles)'; % Primary Key
-table_OpSer.Name         =     OpSer_Name;
-table_OpSer.Shortname    =     OpSer_Shortname;
-table_OpSer.Flag_Typ     = 3 * ones(num_of_profiles,1); % Input Type: 3 - Power (P and Q)
-table_OpSer.Variant_ID   =     ones(num_of_profiles,1);
-table_OpSer.Flag_Variant =     ones(num_of_profiles,1);
-table_OpSer.Tarif        =     ones(num_of_profiles,1);
-table_OpSer.Branch       =     ones(num_of_profiles,1);
-table_OpSer.SubName      =     ones(num_of_profiles,1);
-table_OpSer.Power_a1     =     ones(num_of_profiles,1);
-table_OpSer.Power_b1     =     ones(num_of_profiles,1);
-table_OpSer.Reduce_a2    =     ones(num_of_profiles,1);
-table_OpSer.Reduce_b2    =     ones(num_of_profiles,1);
-table_OpSer.Flag_Ser     =     ones(num_of_profiles,1) * 3; % Series Type: 3 - Yearly
-table_OpSer.BaseT        =     ones(num_of_profiles,1);
+table_OpSer              = table                   ; % initial OpSer table
+table_OpSer.OpSer_ID     = double(1:num_profil)'   ; % Primary Key
+table_OpSer.Name         = OpSer_Name              ;
+table_OpSer.Shortname    = OpSer_Shortname         ;
+table_OpSer.Flag_Typ     = ones(num_profil, 1) * 3 ; % Input Type: 3 - Power (P and Q)
+table_OpSer.Variant_ID   = ones(num_profil, 1)     ;
+table_OpSer.Flag_Variant = ones(num_profil, 1)     ;
+table_OpSer.Tarif        = ones(num_profil, 1)     ;
+table_OpSer.Branch       = ones(num_profil, 1)     ;
+table_OpSer.SubName      = ones(num_profil, 1)     ;
+table_OpSer.Power_a1     = ones(num_profil, 1)     ;
+table_OpSer.Power_b1     = ones(num_profil, 1)     ;
+table_OpSer.Reduce_a2    = ones(num_profil, 1)     ;
+table_OpSer.Reduce_b2    = ones(num_profil, 1)     ;
+table_OpSer.Flag_Ser     = ones(num_profil, 1) * 3 ; % Series Type: 3 - Yearly
+table_OpSer.BaseT        = ones(num_profil, 1)     ;
 
 % Save table
-writetable(table_OpSer,[Sin_PathInput,'OpSer',OpSer_suffix,'.txt'],'Delimiter',';','QuoteStrings',false);
-
-end
+writetable(table_OpSer, [File_Path, 'OpSer', File_suffix, '.txt'] , 'Delimiter', ';' , 'QuoteStrings', false);
