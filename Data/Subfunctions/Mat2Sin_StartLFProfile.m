00f84ec5664394f769a9ec873a37900e56fd8a10
brandalik <brandalik@eit.uni-kl.de>
2018-04-25 18:17:43 +0200

    Revision

--- Data/Subfunctions/Mat2Sin_StartLFProfile.m
+++ Data/Subfunctions/Mat2Sin_StartLFProfile.m
@@ -1,14 +1,14 @@
-function Mat2Sin_StartLFProfile(Sin_Name, Sin_Path, SincalVersion)
+function Mat2Sin_StartLFProfile(GridName, GridPath, SincalVersion)
 %Mat2Sin_StartLFProfile - Start Load-flow with profiles in Sincal
 %
-%   LF_Status = Mat2Sin_StartLF(Sin_Name,Sin_Path)
+%   LF_Status = Mat2Sin_StartLF(GridName,GridPath)
 %
-%       Sin_Name      (Required)  - String that defines the name of the
+%       GridName      (Required)  - String that defines the name of the
 %                                   Sincal file
 %
-%       Sin_Path      (Optional)  - String that defines the path of the
+%       GridPath      (Optional)  - String that defines the path of the
 %                                   Sincal file
-%                                 - (default): 'pwd' - current folder 
+%                                 - (default): 'pwd' - current folder
 %
 %       SincalVersion (Optional)  - Double that defines the Sincal Version
 %                                   installed at your PC
@@ -18,8 +18,8 @@ function Mat2Sin_StartLFProfile(Sin_Name, Sin_Path, SincalVersion)
 
 %% Input check
 
-if nargin < 2; Sin_Path = [pwd,'\']               ; end % Set the default path if no path is given
-if Sin_Path(end) ~= '\'; Sin_Path = [Sin_Path,'\']; end % Correct the path if necessary
+if nargin < 2; GridPath = [pwd,'\']               ; end % Set the default path if no path is given
+if GridPath(end) ~= '\'; GridPath = [GridPath,'\']; end % Correct the path if necessary
 if nargin < 3   % Check installed Sincal Version
     max_Version_Sincal  = 14; max_Version_Mat2Sin = 11; VersionInstalled = false;
     while ~VersionInstalled
@@ -36,40 +36,36 @@ else
     Mat2SinVersion = -17 + 2 * SincalVersion;
 end
 
-%% Matlab connection with the Access DB of the Sincal model 
+%% Matlab connection with the Access DB of the Sincal model
 
-disp(['Starting power flow calculation of ',Sin_Name,'.']);
+disp(['Starting power flow calculation of ',GridName,'.']);
 
-s         = struct                                      ; % Define an object for the connection with Sincal
-s.PathDB  = [Sin_Path, Sin_Name, '_files\database.mdb'] ; % Set the Database path
-s.PathSin = [Sin_Path, Sin_Name,                '.sin'] ; % Set the Sincal path
+DB_Path = [GridPath, GridName, '_files\database.mdb'] ; % Set the Database path
+SinPath = [GridPath, GridName,                '.sin'] ; % Set the Sincal path
 
 try % Setting of the Access COM server, try-catch connection problems
-    s.conn = actxserver(['Sincal.Simulation.',num2str(Mat2SinVersion)]);    % Server for the Matlab connection to Sincal
-    s.conn.Database(['TYP=NET;MODE=JET;FILE=', s.PathDB, ...                % Connection with the Sincal Database
-        ';USR=Admin;PWD=;SINFILE=', s.PathSin, ';']);
+    s = actxserver(['Sincal.Simulation.',num2str(Mat2SinVersion)]);  % Server for the Matlab connection to Sincal
+    s.Database(['TYP=NET;MODE=JET;FILE=', DB_Path, ...               % Connection with the Sincal Database
+        ';USR=Admin;PWD=;SINFILE='      , SinPath, ';']);
 catch
-    error(['Error during the connection of Matlab with Sincal in.', Sin_Name, '.']);
+    error(['Error during the connection of Matlab with Sincal in.', GridName, '.']);
 end
 
-%% Start Load-flow
+CalcMethod = 'LC'; % Set type of calculation, LC - Load Profile
 
 try  % try-catch for errors during load-flow
-    s.strMethod = 'LC';        % Set type of calculation, LC - Load Profile
-    s.conn.Start(s.strMethod); % Start the calculation
-    if s.conn.StatusID == 1101 % Status ID 1101 means successful load flow
-        disp(['Power flow calculation of ',Sin_Name,' successful.']);
+    s.Start(CalcMethod); % Start the calculation
+    if s.StatusID == 1101 % Status ID 1101 means successful load flow
+        disp(['Power flow calculation of ',GridName,' successful.']);
     else % Load Flow was not successful, treat it as error
-        [s.conn] = deal([]); % Stop the Sincal connection, to avoid Matlab from breaking down
-        error(['Power flow calculation of ',Sin_Name,' failed.']);
+        clear s % s = []; % Stop the Sincal connection, to avoid Matlab from breaking down
+        error(['Power flow calculation of ',GridName,' failed.'   ]);
     end
 catch
-    [s.conn] = deal([]); %#ok % Stop the Sincal connection, to avoid Matlab from breaking down
-    error(['Matlab Error occured during ',Sin_Name,' power flow calculation.']);
+    clear s % s = []; % Stop the Sincal connection, to avoid Matlab from breaking down
+    error(['Matlab Error occured during ',GridName,' power flow calculation.']);
 end
+% Stop the Sincal connection, to avoid Matlab from breaking down
+invoke(s,'delete')
+clear s % s = [];
 
-%% Stop the Sincal connection, to avoid Matlab from breaking down
-
-[s.conn] = deal([]); %#ok
-
-end
