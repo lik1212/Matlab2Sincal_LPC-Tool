38d3d8d4d3e03f6fa35e389da14f9d4ab1a2ee4a
brandalik <brandalik@eit.uni-kl.de>
2018-04-27 14:58:55 +0200

    Demonstration File will change from time to time

--- Mat2Sin_LPC_Demo_wo_GUI.m
+++ Mat2Sin_LPC_Demo_wo_GUI.m
@@ -4,30 +4,34 @@
 %% Input settings
 
 Inputs = struct;
-Inputs.Grid_Path          = [pwd, '\Inputs\Grids\'                 ] ;
-Inputs.Grid_Name          = 'S1a_de'                  ;
-Inputs.LP_DB_Path         = [pwd, '\Inputs\Load_Profiles\'         ] ; % Path for load profiles
-Inputs.LP_DB_Type         = 'DB'                                     ;
-Inputs.LP_DB_Name         = 'IEEE_Lo_Profiles.mat'                   ;
-Inputs.LP_dist_path       = [pwd, '\Inputs\Profiles_Distribution\' ] ; % Path for the L.P. distribution file
-Inputs.LP_dist_type       = 'DB_order'                               ;    
-% Inputs.LP_dist_list_name  = 'LoadName_IEEE_LV_Profiles.txt'          ; 
-Inputs.PV_DB_Path         = [pwd, '\Inputs\PV_Profiles\'           ] ; % Path for PV profiles
-Inputs.PV_DB_Type         = 'DB'                                     ;
-Inputs.PV_DB_Name         = 'RB_PV_Profiles.mat'                   ;
-Inputs.PV_dist_path       = [pwd, '\Inputs\Profiles_Distribution\' ] ; % Path for the PV.P. distribution file
-Inputs.PV_dist_type       = 'DB_order'                               ;
-% Inputs.PV_dist_list_name  = 'DCInfeederName_IEEE_LV_Profiles.txt'    ;
+Inputs.Grid_Path          = [pwd, '\Inputs\Grids\'        ];
+Inputs.Grid_Name          = 'S1a_de'                       ;
+Inputs.LP_DB_Path         = [pwd, '\Inputs\Load_Profiles\']; % Path for load profiles
+Inputs.LP_DB_Type         = 'DB'                           ;
+Inputs.LP_DB_Name         = 'IEEE_Lo_Profiles.mat'         ;
+Inputs.LP_dist_type       = 'DB_order'                     ;    
+Inputs.PV_DB_Path         = [pwd, '\Inputs\PV_Profiles\'  ]; % Path for PV profiles
+Inputs.PV_DB_Type         = 'DB'                           ;
+Inputs.PV_DB_Name         = 'RB_PV_Profiles.mat'           ;
+Inputs.PV_dist_type       = 'DB_order'                     ;
 
 Inputs.VerSincal          = 13  ;
 Inputs.ParrallelCom       = true;
 Inputs.NumelCores         = 3  ;
-% Inputs.waitbar_activ      = false;
 
 Inputs.Outputs_Path       = [pwd, '\Outputs\'         ]   ; % Path for load profiles
-Inputs.Output_option_raw  = true;    % TODO: Improve the correction of other flags if this is true
-% Inputs.Output_option_raw_only        = true;    % TODO: Improve the correction of other flags if this is true
+
+% If LP_dist_type or PV_dist_type is set to 'list', define the list:
+% Inputs.LP_dist_path       = [pwd, '\Inputs\Profiles_Distribution\' ] ; % Path for the L.P. distribution file
+% Inputs.LP_dist_list_name  = 'LoadName_IEEE_LV_Profiles.txt'          ; 
+% Inputs.PV_dist_path       = [pwd, '\Inputs\Profiles_Distribution\' ] ; % Path for the PV.P. distribution file
+% Inputs.PV_dist_list_name  = 'DCInfeederName_IEEE_LV_Profiles.txt'    ;
+
+% Example of other functions:
+% Inputs.Output_option_raw      = true ;
+% Inputs.waitbar_activ          = false;
+% Inputs.Output_option_raw_only = true ;
 
 %% Start function
 
-status = Mat2Sin_LPCalc(Inputs)
+status = Mat2Sin_LPCalc(Inputs);
