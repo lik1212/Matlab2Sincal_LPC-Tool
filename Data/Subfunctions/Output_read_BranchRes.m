ce91e552297ad71297aee8c941614f14fbb45ff1
brandalik <brandalik@eit.uni-kl.de>
2018-04-30 09:26:08 +0200

    If the number of time steps and nodes is very big, the results cannot contain the name of nodes and elements because results will exceed the RAM limits. So only the IDs of Nodes and Elements will be saved. For this reason, some changes in the code were necessary. Also, the saved results per units will now be split into results in bout line directions (IDs increasing and decreasing).

--- Data/Subfunctions/Output_read_BranchRes.m
+++ Data/Subfunctions/Output_read_BranchRes.m
@@ -62,29 +62,25 @@ NaN_Table_Branches.Terminal2_ID = repelem(Branch_lack_set(:,2), numel(Step_all) 
 ResData = [ResData; NaN_Table_Steps; NaN_Table_Branches];
 ResData = sortrows(ResData,'Terminal2_ID','ascend');
 ResData = sortrows(ResData,'Terminal1_ID','ascend');
-ResData = sortrows(ResData,'ResTime','ascend');
+ResData = sortrows(ResData,'ResTime'     ,'ascend');
 
 %% Add Element Node_IDs, Node_Names, Element_ID and Element_Names
+
+% Initial
+ResData.Node1_ID   = zeros(size(ResData, 1), 1);
+ResData.Node2_ID   = zeros(size(ResData, 1), 1);
+ResData.Element_ID = zeros(size(ResData, 1), 1);
 
 Occurred_Terminals = unique([ResData.Terminal1_ID; ResData.Terminal2_ID]);
 for k_Branch = 1 : numel(SinInfo.Terminal.Terminal_ID) % Very time consuming script
     if ismember(SinInfo.Terminal.Terminal_ID(k_Branch), Occurred_Terminals)
-        Terminal1_ID_flag = (ResData.Terminal1_ID       == SinInfo.Terminal.Terminal_ID(k_Branch))  ;
-        Terminal2_ID_flag = (ResData.Terminal2_ID       == SinInfo.Terminal.Terminal_ID(k_Branch))  ;
-        Node_ID_flag      = (SinInfo.Node.Node_ID       == SinInfo.Terminal.Node_ID    (k_Branch))  ;
-        Element_ID_flag   = (SinInfo.Element.Element_ID == SinInfo.Terminal.Element_ID (k_Branch))  ;
-        ResData.Node1_ID    (Terminal1_ID_flag) = SinInfo.Terminal.Node_ID   (k_Branch)             ;
-        ResData.Node2_ID    (Terminal2_ID_flag) = SinInfo.Terminal.Node_ID   (k_Branch)             ;
-        ResData.Element_ID  (Terminal1_ID_flag) = SinInfo.Terminal.Element_ID(k_Branch)             ;
-        ResData.Node1_Name  (Terminal1_ID_flag) = SinInfo.Node.Name(Node_ID_flag)                   ;
-        ResData.Node2_Name  (Terminal2_ID_flag) = SinInfo.Node.Name(Node_ID_flag)                   ;
-        ResData.Element_Name(Terminal1_ID_flag) = SinInfo.Element.Name(Element_ID_flag)             ;
+        Terminal1_ID_flag = (ResData.Terminal1_ID == SinInfo.Terminal.Terminal_ID(k_Branch));
+        Terminal2_ID_flag = (ResData.Terminal2_ID == SinInfo.Terminal.Terminal_ID(k_Branch));
+        ResData.Node1_ID  (Terminal1_ID_flag) = SinInfo.Terminal.Node_ID   (k_Branch)       ;
+        ResData.Node2_ID  (Terminal2_ID_flag) = SinInfo.Terminal.Node_ID   (k_Branch)       ;
+        ResData.Element_ID(Terminal1_ID_flag) = SinInfo.Terminal.Element_ID(k_Branch)       ;
     end
 end
-
-% clear useless data to save space
-SimData_ID_up.  Terminal1_ID = []; SimData_ID_up.  Terminal2_ID = [];
-SimData_ID_down.Terminal1_ID = []; SimData_ID_down.Terminal2_ID = [];
 
 %% Saving only RAW data in a file and leaving function
 
@@ -100,18 +96,6 @@ if Settings.Output_option_raw_only || Settings.Output_option_raw
     end
     if Settings.Output_option_raw_only; return; end
 end
-
-%% Adapting Current flows
-
-% ID_up means the Terminal_ID is smaller, vice-verse for ID_down
-SimData_ID_up   = BranchRes_all(BranchRes_all.Terminal1_ID < BranchRes_all.Terminal2_ID,:);
-SimData_ID_down = BranchRes_all(BranchRes_all.Terminal1_ID > BranchRes_all.Terminal2_ID,:);
-
-clear BranchRes_all;  % To reduce RAM usage
-
-% clear useless data to save space
-SimData_ID_up.  Terminal1_ID = []; SimData_ID_up.  Terminal2_ID = [];
-SimData_ID_down.Terminal1_ID = []; SimData_ID_down.Terminal2_ID = [];
 
 %% Read out flag and make column name translator
 
@@ -150,14 +134,38 @@ end
 
 all_flag = [U_flag; P_flag; Q_flag; S_flag];
 
+
+%% Adapting Current flows
+
+% ID_up means the Terminal_ID is smaller, vice-verse for ID_down
+SimData_ID_up   = BranchRes_all(BranchRes_all.Terminal1_ID < BranchRes_all.Terminal2_ID,:);
+SimData_ID_down = BranchRes_all(BranchRes_all.Terminal1_ID > BranchRes_all.Terminal2_ID,:);
+
+clear BranchRes_all;  % To reduce RAM usage
+
+% clear useless data to save space
+SimData_ID_up.  Terminal1_ID = []; SimData_ID_up.  Terminal2_ID = [];
+SimData_ID_down.Terminal1_ID = []; SimData_ID_down.Terminal2_ID = [];
+
 %% Name and ID prepeartion
 % Column names of tables can not start with numbers or have some special
 % characters like '-', this section will try to avoid such problems.
 
-Branch_IDs = SimData_ID_down.Element_ID(1:size(Branch_all, 1));
-BranchNames     = strrep(SimData_ID_down.Element_Name(1:numel(Branch_IDs)), '-', '');  % Initialisierung
-Node1_Name_up   = strrep(SimData_ID_up.  Node1_Name  (1:numel(Branch_IDs)), '-', '');  % Initialisierung
-Node1_Name_down = strrep(SimData_ID_down.Node1_Name  (1:numel(Branch_IDs)), '-', '');  % Initialisierung
+Branch_IDs      = SimData_ID_down.Element_ID(1 : size(Branch_all, 1));
+Node1_IDs_up    = SimData_ID_up  .Node1_ID  (1 : size(Branch_all, 1));
+Node1_IDs_down  = SimData_ID_down.Node1_ID  (1 : size(Branch_all, 1)); 
+BranchNames     = strings(numel(Branch_IDs), 1);  % Initialisierung
+Node1_Name_up   = strings(numel(Branch_IDs), 1);  % Initialisierung
+Node1_Name_down = strings(numel(Branch_IDs), 1);  % Initialisierung
+for k_Branch = 1 : numel(Branch_IDs)
+    Element_ID_flag    = (SinInfo.Element.Element_ID == Branch_IDs    (k_Branch));
+    Node1_ID_up_flag   = (SinInfo.Node.Node_ID       == Node1_IDs_up  (k_Branch));
+    Node1_ID_down_flag = (SinInfo.Node.Node_ID       == Node1_IDs_down(k_Branch));
+    BranchNames    (k_Branch) = strrep(SinInfo.Element.Name(Element_ID_flag   ), '-', '');
+    Node1_Name_up  (k_Branch) = strrep(SinInfo.Node.   Name(Node1_ID_up_flag  ), '-', '');
+    Node1_Name_down(k_Branch) = strrep(SinInfo.Node.   Name(Node1_ID_down_flag), '-', '');
+end
+
 BranchVarNames  = strrep(                                              ...
     strcat('ID',num2str(double(1:numel(Branch_IDs))'),'_',BranchNames)  ...
     ,' ','');
@@ -171,33 +179,30 @@ for k_flag = 1 : size(all_flag, 1)
         % Variable value from smaller to bigger Terminal_ID    
         Element_flag = (SimData_ID_up.  Element_ID == Branch_IDs(k_Branch));
         Vari_up   = SimData_ID_up.  (all_flag{k_flag, 1})(Element_flag);
-        Vari_down = SimData_ID_down.(all_flag{k_flag, 1})(Element_flag);
-        % Maximum absolute variable value           
-        Vari_abs_max = NaN(numel(Step_all),1); % Initialisierung
-        Vari_abs_max(abs(Vari_down) >= abs(Vari_up  )) = abs(Vari_down(abs(Vari_down) >= abs(Vari_up  )));
-        Vari_abs_max(abs(Vari_up  ) >= abs(Vari_down)) = abs(Vari_up  (abs(Vari_up  ) >= abs(Vari_down)));       
+        Vari_down = SimData_ID_down.(all_flag{k_flag, 1})(Element_flag);   
         if Settings.Output_option_per_unit
             if k_Branch == 1
                 if k_flag == 1
-                    SimResults_Branches_per_units = struct; % initial
+                    SimResults_Branches_per_units_ID_up   = struct; % initial
+                    SimResults_Branches_per_units_ID_down = struct; % initial
                 end               
-                SimResults_Branches_per_units.(all_flag{k_flag, 2}) = table;  % initial as table
+                SimResults_Branches_per_units_ID_up.  (all_flag{k_flag, 2}) = table;  % initial as table
+                SimResults_Branches_per_units_ID_down.(all_flag{k_flag, 2}) = table;  % initial as table
             end
-            SimResults_Branches_per_units.(all_flag{k_flag, 2}).(BranchVarNames{k_Branch}) = Vari_abs_max;
+            SimResults_Branches_per_units_ID_up.  (all_flag{k_flag, 2}).(BranchVarNames{k_Branch}) = Vari_up  ;
+            SimResults_Branches_per_units_ID_down.(all_flag{k_flag, 2}).(BranchVarNames{k_Branch}) = Vari_down;
         end
         if Settings.Output_option_per_node_branch
             if k_flag == 1
                 if k_Branch == 1
-                    SimResults_Branches_per_branches = struct; % initial
+                    SimResults_Branches_per_branches   = struct; % initial
                 end
                 SimResults_Branches_per_branches.(BranchNames{k_Branch}) = struct; % initial
                 SimResults_Branches_per_branches.(BranchNames{k_Branch}).(Node1_Name_up  {k_Branch}) = table; % initial as table
                 SimResults_Branches_per_branches.(BranchNames{k_Branch}).(Node1_Name_down{k_Branch}) = table;
-                SimResults_Branches_per_branches.(BranchNames{k_Branch}).HighestAbsolute             = table;
             end
-            SimResults_Branches_per_branches.(BranchNames{k_Branch}).(Node1_Name_up  {k_Branch}).(all_flag{k_flag, 2}) = Vari_up      ;
-            SimResults_Branches_per_branches.(BranchNames{k_Branch}).(Node1_Name_down{k_Branch}).(all_flag{k_flag, 2}) = Vari_down    ;
-            SimResults_Branches_per_branches.(BranchNames{k_Branch}).HighestAbsolute            .(all_flag{k_flag, 2}) = Vari_abs_max ;
+            SimResults_Branches_per_branches.(BranchNames{k_Branch}).(Node1_Name_up  {k_Branch}).(all_flag{k_flag, 2}) = Vari_up  ;
+            SimResults_Branches_per_branches.(BranchNames{k_Branch}).(Node1_Name_down{k_Branch}).(all_flag{k_flag, 2}) = Vari_down;
         end
     end
     SimData_ID_up.  (all_flag{k_flag, 1}) = []; % To reduce RAM usage
@@ -208,21 +213,35 @@ clear SimData_ID_down SimData_ID_up % To reduce RAM usage
 %% Saving the results in .mat files
 
 if Settings.Output_option_per_unit
-    SimData_Filename = [Path_Output, SinNameBasic, '_BranchRes_per_units.mat'];
-    SimResults_Branches_per_units_Bytes = whos('SimResults_Branches_per_units');
-    if ~isempty(SimResults_Branches_per_units_Bytes)
-        SimResults_Branches_per_units_Bytes = SimResults_Branches_per_units_Bytes.bytes; % The variable will just be saved
-        if SimResults_Branches_per_units_Bytes > 2 * 1024^3
-            save(SimData_Filename, 'SimResults_Branches_per_units', '-v7.3');
+    SimData_Filename_ID_up = [Path_Output, SinNameBasic, '_BranchRes_per_units_ID_up.mat'];
+    SimResults_Branches_per_units_ID_up_Bytes = whos('SimResults_Branches_per_units_ID_up');
+    if ~isempty(SimResults_Branches_per_units_ID_up_Bytes)
+        SimResults_Branches_per_units_ID_up_Bytes = SimResults_Branches_per_units_ID_up_Bytes.bytes; % The variable will just be saved
+        if SimResults_Branches_per_units_ID_up_Bytes > 2 * 1024^3
+            save(SimData_Filename_ID_up, 'SimResults_Branches_per_units_ID_up', '-v7.3');
         else
-            save(SimData_Filename, 'SimResults_Branches_per_units'         );
+            save(SimData_Filename_ID_up, 'SimResults_Branches_per_units_ID_up'         );
         end
-        disp([SimData_Filename, ' saved.']);
+        disp([SimData_Filename_ID_up, ' (ID_up) saved.']);
     end
+    clear SimResults_Branches_per_units_ID_up % To reduce RAM usage
+    
+    SimData_Filename_ID_down = [Path_Output, SinNameBasic, '_BranchRes_per_units_ID_down.mat'];
+    SimResults_Branches_per_units_ID_down_Bytes = whos('SimResults_Branches_per_units_ID_down');
+    if ~isempty(SimResults_Branches_per_units_ID_down_Bytes)
+        SimResults_Branches_per_units_ID_down_Bytes = SimResults_Branches_per_units_ID_down_Bytes.bytes; % The variable will just be saved
+        if SimResults_Branches_per_units_ID_down_Bytes > 2 * 1024^3
+            save(SimData_Filename_ID_down, 'SimResults_Branches_per_units_ID_down', '-v7.3');
+        else
+            save(SimData_Filename_ID_down, 'SimResults_Branches_per_units_ID_down'         );
+        end
+        disp([SimData_Filename_ID_down, ' (ID_down) saved.']);
+    end
+    clear SimResults_Branches_per_units_ID_down % To reduce RAM usage
 end
 if Settings.Output_option_per_node_branch
     SimData_Filename = [Path_Output, SinNameBasic, '_BranchRes_per_branches.mat'];
-    SimResults_Branches_per_branches_Bytes = whos('SimResults_Branches_per_units');
+    SimResults_Branches_per_branches_Bytes = whos('SimResults_Branches_per_branches');
     if ~isempty(SimResults_Branches_per_branches_Bytes)
         SimResults_Branches_per_branches_Bytes = SimResults_Branches_per_branches_Bytes.bytes; % The variable will just be saved
         if SimResults_Branches_per_branches_Bytes > 2 * 1024^3
